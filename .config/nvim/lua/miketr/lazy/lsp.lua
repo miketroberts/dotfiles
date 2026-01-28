c755da82117333057976fbbdf64262e8f2505080
Mike Roberts <mike@mikeroberts.org>
2026-01-28 14:01:11 -0800

    LSP keybindings.

--- .config/nvim/lua/miketr/lazy/lsp.lua
+++ .config/nvim/lua/miketr/lazy/lsp.lua
@@ -26,7 +26,37 @@ return {
     "mfussenegger/nvim-jdtls",
     "mfussenegger/nvim-dap",
   },
-
+  opts = {
+    ---@class PluginLspOpts
+    servers = {
+      -- configuration for all lsp servers
+      ["*"] = {
+        capabilities = {
+          workspace = {
+            fileOperations = {
+              didRename = true,
+              willRename = true,
+            },
+          },
+        },
+        -- stylua: ignore
+        keys = {
+          -- { "gd",         vim.lsp.buf.definition,                             desc = "Goto Definition",            has = "definition" },
+          -- { "gr",         vim.lsp.buf.references,                             desc = "References",                 nowait = true },
+          { "gI",         vim.lsp.buf.implementation,                         desc = "Goto Implementation" },
+          { "gy",         vim.lsp.buf.type_definition,                        desc = "Goto T[y]pe Definition" },
+          { "gD",         vim.lsp.buf.declaration,                            desc = "Goto Declaration" },
+          { "K",          function() return vim.lsp.buf.hover() end,          desc = "Hover" },
+          { "gK",         function() return vim.lsp.buf.signature_help() end, desc = "Signature Help",             has = "signatureHelp" },
+          { "<c-k>",      function() return vim.lsp.buf.signature_help() end, mode = "i",                          desc = "Signature Help", has = "signatureHelp" },
+          { "<leader>ca", vim.lsp.buf.code_action,                            desc = "Code Action",                mode = { "n", "x" },     has = "codeAction" },
+          { "<leader>cc", vim.lsp.codelens.run,                               desc = "Run Codelens",               mode = { "n", "x" },     has = "codeLens" },
+          { "<leader>cC", vim.lsp.codelens.refresh,                           desc = "Refresh & Display Codelens", mode = { "n" },          has = "codeLens" },
+          { "<leader>cr", vim.lsp.buf.rename,                                 desc = "Rename",                     has = "rename" },
+        },
+      },
+    }
+  },
   config = function()
     require("conform").setup({
       formatters_by_ft = {
