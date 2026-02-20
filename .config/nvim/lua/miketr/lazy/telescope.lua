ba0f142299695db341444e9380cb988c43928636
Mike Roberts <mike@mikeroberts.org>
2026-02-20 13:23:00 -0800

    Enable live search.

--- .config/nvim/lua/miketr/lazy/telescope.lua
+++ .config/nvim/lua/miketr/lazy/telescope.lua
@@ -15,10 +15,10 @@ return {
     require('telescope').setup({
       extensions = {
         fzf = {
-          fuzzy = true,                     -- false will only do exact matching
-          override_generic_sorter = true,   -- override the generic sorter
-          override_file_sorter = true,      -- override the file sorter
-          case_mode = "smart_case",         -- or "ignore_case" or "respect_case"
+          fuzzy = true,                   -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true,    -- override the file sorter
+          case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
           -- the default case_mode is "smart_case"
         }
       }
@@ -37,9 +37,7 @@ return {
       local word = vim.fn.expand('<cWORD>')
       builtin.grep_string({ search = word }, { desc = 'Search [F]iles for [W]ORDR under cursor' })
     end)
-    vim.keymap.set('n', '<leader>fg', function()
-      builtin.grep_string({ search = vim.fn.input("Grep > ") }, { desc = 'Search [F]iles with [G]rep' })
-    end)
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Search [F]iles with [G]rep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Search [F]iles [Buffers]' })
   end
 }
