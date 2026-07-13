3309886626c6b843e0fcc63c7e8b4ae4c00e6ebc
Mike Roberts <mike.roberts@vanta.com>
2026-07-13 14:32:21 -0700

    Automatically install nvim/tmux plugins.

--- install.sh
+++ install.sh
@@ -58,3 +58,7 @@ make install
 # Link in all of the configuration.
 script_dir=$(dirname "$(readlink -f "$0")")
 sync_dotfiles $script_dir ~ false
+
+# Post install
+nvim --headless "+Lazy! install" +qa
+~/.tmux/plugins/tpm/bin/install_plugins
