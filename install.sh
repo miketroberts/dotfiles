3b9ba3f3cf49506ac2c4fc15130cb259ecf7e7e0
Mike Roberts <mike.roberts@vanta.com>
2026-07-13 20:58:17 -0700

    Fixing path.... again.

--- install.sh
+++ install.sh
@@ -60,5 +60,5 @@ script_dir=$(dirname "$(readlink -f "$0")")
 sync_dotfiles $script_dir ~ false
 
 # Post install
-~/local/bin/nvim --headless "+Lazy! install" +qa
+~/local/nvim/bin/nvim --headless "+Lazy! install" +qa
 ~/.tmux/plugins/tpm/bin/install_plugins
